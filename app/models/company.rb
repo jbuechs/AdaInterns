1c12a5ea743d49e09d415005f947708e7027afb4
Jennie Buechner <janina.buechner@gmail.com>
2015-11-25 09:02:40 -0800

    include interns and cohorts on company page

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,18 @@ class Company < ActiveRecord::Base
   has_many :interns, dependent: :destroy
   has_many :employees
   validates :name, presence: true
+
+  def num_interns
+    prev_interns = self.interns.count
+    self.sponsor_four.nil? ? (prev_interns) : (prev_interns + self.sponsor_four)
+  end
+
+  def which_cohorts
+    cohorts = []
+    self.interns.each do |intern|
+      cohorts.push(intern.cohort)
+    end
+    cohorts.push(4) if !self.sponsor_four.nil?
+    return cohorts.uniq
+  end
 end

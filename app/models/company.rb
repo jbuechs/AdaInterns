9383027d2a38b6f743b8ac028adc4e8de73f71ba
Jennie Buechner <janina.buechner@gmail.com>
2015-11-27 12:48:16 -0800

    add filterrific to company model

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,11 +5,25 @@ class Company < ActiveRecord::Base
   has_many :employees
   validates :name, presence: true
 
+
+  # define ActiveRecord scopes for
+  # :sorted_by, :search_query, :with_cohort_id
+  filterrific(
+  default_filter_params: { sorted_by: 'name_asc' },
+  available_filters: [
+    :sorted_by,
+    :search_query,
+    :with_cohort_id
+  ]
+)
+
+  # returns the number of interns, including those for cohort 4
   def num_interns
     prev_interns = self.interns.count
     self.sponsor_four.nil? ? (prev_interns) : (prev_interns + self.sponsor_four)
   end
 
+  # returns an array of which cohorts the company has sponsored
   def which_cohorts
     cohorts = []
     self.interns.each do |intern|
@@ -19,6 +33,7 @@ class Company < ActiveRecord::Base
     return cohorts.uniq
   end
 
+  # returns a hash of results of a query of the company's Glassdoor info
   def getGlassdoor
     response = open("http://api.glassdoor.com/api/api.htm?t.p=49111&t.k=kHImtMBuowe&userip=0.0.0.0&useragent=&format=json&v=1&action=employers&q=#{self.name}&city=Seattle").read
     parsed_json = ActiveSupport::JSON.decode(response)

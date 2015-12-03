475c7a2b3b1d604a495146130dee3b5d2bb15a35
Jennie Buechner <janina.buechner@gmail.com>
2015-12-03 08:37:41 -0800

    change email addresses for uniqueness

--- app/models/intern.rb
+++ app/models/intern.rb
@@ -6,7 +6,7 @@ class Intern < ActiveRecord::Base
   validates :first_name, presence: true, length: { maximum: 30 }
   validates :last_name, presence: true, length: { maximum: 30 }
   validates :cohort, presence: true
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true,
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }

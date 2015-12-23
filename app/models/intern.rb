807035721bf077b5d992ee1c3c8aba125c63e513
Jennie Buechner <janina.buechner@gmail.com>
2015-12-23 11:02:05 -0800

    add devise to intern model

--- app/models/intern.rb
+++ app/models/intern.rb
@@ -1,4 +1,8 @@
 class Intern < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :remember_token
 
   belongs_to :company
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  devise_for :interns
   get 'sessions/new'
 
   root 'static_pages#index'

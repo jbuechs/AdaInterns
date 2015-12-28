125ebd274a343e06e1f9336aac12fbe13675300e
Jennie Buechner <janina.buechner@gmail.com>
2015-12-28 10:48:26 -0800

    begin employee model tests

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".validates" do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:position) }
+    it { is_expected.to validate_presence_of(:company_id) }
+  end
 end

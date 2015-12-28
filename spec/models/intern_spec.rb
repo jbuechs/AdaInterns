eeda6ac801e5dbc76cf51f746c8629b598600d19
Jennie Buechner <janina.buechner@gmail.com>
2015-12-28 10:51:03 -0800

    begin spec for intern model

--- spec/models/intern_spec.rb
+++ spec/models/intern_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Intern, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".validates" do
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:cohort) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
+  end
 end
